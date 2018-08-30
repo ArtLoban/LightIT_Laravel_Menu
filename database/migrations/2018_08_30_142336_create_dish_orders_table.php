07118b7a3f5c5596da29babc7e2319cab3a04c5f
Artem Loban <tymanrock@gmail.com>
2018-08-30 17:35:45 +0300

    In progress 40

---
+++ database/migrations/2018_08_30_142336_create_dish_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDishOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dish_orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dish_orders');
+    }
+}
